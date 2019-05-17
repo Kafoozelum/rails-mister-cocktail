fe6a8d81417222a9398ff7d926df7a737f829e60
Julian Hermann <julian.hermann89@gmail.com>
2019-05-17 17:35:37 +0200

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'json'
+# require 'open-uri'
+
+
+# open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list") {|f|
+#   f.each_line {|line| p line}
+# }
+
+Ingredient.create(name: "IngredientOne")
+Ingredient.create(name: "IngredientTwo")
+Ingredient.create(name: "IngredientThree")
+
+Dose.create(description: "DoseOne")
+Dose.create(description: "DoseTwo")
+Dose.create(description: "DoseThree")
+
+
+Cocktail.delete_all
+
+Cocktail.create!(
+  [
+    {
+      name: "CocktailOne"
+    },
+    {
+      name: "CocktailTwo"
+    },
+    {
+      name: "CocktailThree"
+    },
+  ]
+)
